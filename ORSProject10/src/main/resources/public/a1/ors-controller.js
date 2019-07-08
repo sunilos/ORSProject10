4ccc8ee8b4d9ea5506fa651c31f1b487b0c4190d
SunilOS <sunilos007@gmail.com>
2019-07-08 18:17:20 +0530

    RoleCrl changed

--- ORSProject10/src/main/resources/public/a1/ors-controller.js
+++ ORSProject10/src/main/resources/public/a1/ors-controller.js
@@ -215,8 +215,57 @@ app.controller('userListCtl', function($scope, $routeParams, ServiceLocator) {
 app.controller('roleCtl', function($scope, $routeParams, ServiceLocator) {
 
 	_self = this;
+	
+	/**
+	 * Server API to call
+	 */
+	_self.api = ServiceLocator.endpointService.getAPI( ServiceLocator.endpointService.Role);
 
-	initController(_self, ServiceLocator.endpointService.Role, $scope, $routeParams, ServiceLocator);
+	/**
+	 * Initialize single record controller
+	 */
+	_self.init = function() {
+		$scope.form.data.id = 0;
+		if ($routeParams.id) {
+			$scope.form.data.id = $routeParams.id;
+		}
+		$scope.display();
+		$scope.preload();
+	}
+
+	
+	/**
+	 * Initialize list controller
+	 */
+	_self.initList = function() {
+		$scope.search();
+		$scope.preload();
+	}
+
+	/**
+	 * Populate bean from response data
+	 */
+	ctl.populateForm  = function(form, data) {
+	}
+
+	/**
+	 * HTML Form data
+	 */
+	$scope.form = {
+		error : false, // error
+		message : '', // error or sucess message
+		preload : null, // preload data
+		data : {
+			id : null
+		}, // form data
+		inputerror : {}, // form input error messages
+		searchParams : {}, // search form
+		searchMessage : null, // search result message
+		list : [], // search list
+		pageNo : 0,
+		pageSize : 5
+	};
+
 	
 	_self.populateForm  = function(form, data) {
 		form.id = data.id;
@@ -224,6 +273,102 @@ app.controller('roleCtl', function($scope, $routeParams, ServiceLocator) {
 		form.discription = data.discription;
 		console.log('Populated Form', form, _self.api);
 	}
+	
+	
+	/**
+	 * Fetches a record by primary key
+	 */
+	$scope.display = function() {
+		if ($scope.form.data.id > 0) {
+			var url = _self.api.get + "/" + $scope.form.data.id;
+			ServiceLocator.http.get(url, function(response) {
+				$scope.form.error = !response.success;
+				$scope.form.message = response.result.message;
+				ctl.populateForm($scope.form.data, response.result.data);
+			});
+		}
+	}	
+
+	// Contains display logic
+	
+	/**
+	 * Fetches preload data
+	 */
+	$scope.preload = function() {
+		console.log(_self.api.preload);
+		ServiceLocator.http.get(_self.api.preload, function(response) {
+			$scope.form.preload = response.result;
+		});
+	}	
+	
+	/**
+	 * Save a record
+	 */
+	$scope.submit = function() {
+		ServiceLocator.http.post(_self.api.save, $scope.form.data,
+				function(response) {
+					$scope.form.error = !response.success;
+					
+					if(response.success){
+						$scope.form.message = "Data is saved";
+					}else{
+						$scope.form.message = response.result.message;
+						$scope.form.inputerror = response.inputerror;
+					}
+				});
+	}
+	
+	/**
+	 * Deletes a record
+	 */
+
+	$scope.delete = function() {
+		if ($scope.form.data.id > 0) {
+			var url = api.delete + "/" + $scope.form.data.id;
+			ServiceLocator.http.get(url, function(response) {
+				$scope.form.error = !response.success;
+				$scope.form.message = response.result.message;
+				populateForm($scope.form.data, response.result.data);
+			});
+		}
+	}
+
+	/**
+	 * Makes search http call
+	 */
+	
+	$scope.search = function() {
+		var url = _self.api.search + "/" + $scope.form.pageNo;
+		ServiceLocator.http.post(url, $scope.form.searchParams, function(
+				response) {
+			$scope.form.error = !response.success;
+			$scope.form.searchMessage = response.result.message;
+			$scope.form.list = response.result.data;
+			if ($scope.form.list.length == 0) {
+				$scope.form.searchMessage = "No record found";
+			}
+		});
+	}
+
+
+	/**
+	 * Navigate to next page
+	 */
+	$scope.forward = function(page) {
+		ServiceLocator.locationService.url(page);
+	}
+
+	$scope.next = function() {
+		$scope.form.pageNo++;
+		$scope.search();
+	}
+
+	$scope.previous = function() {
+		if ($scope.form.pageNo > 0) {
+			$scope.form.pageNo--;
+			$scope.search();
+		}
+	}	
 	
 	_self.init();
 });
